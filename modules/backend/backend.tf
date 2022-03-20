c6557946695ccabdc5800dc6304f4e555088c6e1
utsa <utsav>
2022-03-20 20:20:02 +0530

    backend module created

---
+++ modules/backend/backend.tf
@@ -0,0 +1,12 @@
+
+variable "my_rg" {
+  description = "RG name"
+}
+variable "rg_name" {
+  description = "rg name from root main"
+  default = "utsav_rg"
+}
+variable "my_loc" {
+  description = "rg location from root main"
+  default = "utsav"
+}
---
+++ modules/backend/variables.tf
@@ -0,0 +1,20 @@
+locals {
+  rg_name = var.my_rg
+  my_loc = var.my_loc
+}
+
+# Create a resource group
+resource "azurerm_resource_group" "preet" {
+  name     = local.rg_name
+  location = local.my_loc
+}
+
+resource "azurerm_storage_account" "backend" {
+  name                     = "utsav"
+  resource_group_name      = local.rg_name
+  location                 = local.my_loc
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+
+
+} 
